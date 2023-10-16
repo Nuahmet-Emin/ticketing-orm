d5b5a362649b61c1fb5431c5ac96f4b2266a945d
nurahmetemin <nurahmedemin@gmail.com>
2023-10-15 22:53:17 -0700

    added repository for project service

---
+++ src/main/java/com/cydeo/repository/ProjectRepository.java
@@ -0,0 +1,11 @@
+package com.cydeo.repository;
+
+import com.cydeo.entity.Project;
+import com.cydeo.entity.User;
+import org.hibernate.metamodel.model.convert.spi.JpaAttributeConverter;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProjectRepository extends JpaRepository<Project,Long> {
+
+    Project findByProjectCode(String code);
+}
